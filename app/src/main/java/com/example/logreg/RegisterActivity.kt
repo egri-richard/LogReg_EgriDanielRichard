0b1cc680717e9d8f9ebae3c7137220953901fb93
Erinad-2002 <Erinad.business@gmail.com>
2021-12-04 16:53:39 +0100

    color change in inputs and validation

--- app/src/main/java/com/example/logreg/RegisterActivity.kt
+++ app/src/main/java/com/example/logreg/RegisterActivity.kt
@@ -1,6 +1,7 @@
 package com.example.logreg
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,6 +15,7 @@ class RegisterActivity : AppCompatActivity() {
     private lateinit var regFullNameET : EditText
     private lateinit var regBtn : Button
     private lateinit var backFromRegBtn : Button
+    private val db = DBHandler(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,32 +27,85 @@ class RegisterActivity : AppCompatActivity() {
             finish()
         }
 
-        regBtn.setOnClickListener {
-            val db = DBHandler(this)
-
-            val uName = regFelhnevET.text.toString()
-            val Email = regEmailET.text.toString()
-
-            val validUName = !db.containsUsername(uName)
-            val validEmail = !db.containsEmail(Email)
-
-            if (validUName && validEmail) {
-                db.insert(User(
-                    regFullNameET.text.toString(),
-                    regFelhnevET.text.toString(),
-                    regPassET.text.toString(),
-                    regEmailET.text.toString()
-                ))
-
-                Toast.makeText(applicationContext, "User added to database", Toast.LENGTH_SHORT).show()
-            } else {
-                if (validUName) {
-                    Toast.makeText(applicationContext, "This Email address is already in use", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(applicationContext, "This Username is already in use", Toast.LENGTH_SHORT).show()
+        regEmailET.setOnFocusChangeListener { view, b ->
+            val text = regEmailET.text.toString()
+            if (!regEmailET.isFocused) {
+                if (db.containsEmail(text)) {
+                    regEmailET.setTextColor(Color.parseColor("#FF0000"))
                 }
+            } else {
+                regEmailET.setTextColor(Color.parseColor("#000000"))
             }
         }
+
+        regFelhnevET.setOnFocusChangeListener { view, b ->
+            val text = regFelhnevET.text.toString()
+            if (!regFelhnevET.isFocused) {
+                if (db.containsUsername(text)) {
+                    regFelhnevET.setTextColor(Color.parseColor("#FF0000"))
+                }
+            } else {
+                regFelhnevET.setTextColor(Color.parseColor("#000000"))
+            }
+        }
+
+        regBtn.setOnClickListener {
+            if (validation()) {
+                db.insert(User(
+                    regEmailET.text.toString(),
+                    regFelhnevET.text.toString(),
+                    regPassET.text.toString(),
+                    regFullNameET.text.toString()
+                ))
+            }
+        }
+    }
+
+    private fun validation() : Boolean {
+        var retBool = true
+
+        //email validation
+        val emailText = regEmailET.text.toString()
+        if (!emailText.contains(".")) {
+            retBool = false
+            Toast.makeText(applicationContext, "Wrong email format", Toast.LENGTH_SHORT).show()
+            return retBool
+        } else if(!emailText.contains("@")) {
+            retBool = false
+            Toast.makeText(applicationContext, "Wrong email format", Toast.LENGTH_SHORT).show()
+            return retBool
+        } else if (db.containsEmail(emailText)) {
+            retBool = false
+            Toast.makeText(applicationContext, "This email is already in use", Toast.LENGTH_SHORT).show()
+            return retBool
+        }
+
+        //username validation
+        val uName = regFelhnevET.text.toString()
+        if (db.containsUsername(uName)) {
+            retBool = false
+            Toast.makeText(applicationContext, "This username already exists", Toast.LENGTH_SHORT).show()
+            return retBool
+        }
+
+        //full name validation
+        val fullName = regFullNameET.text.toString()
+        val fnWords = fullName.split(" ").toMutableList()
+        if(fnWords.count() > 1) {
+            for (word in fnWords) {
+                if (!word.first().isUpperCase()) {
+                    retBool = false
+                    Toast.makeText(applicationContext, "Your name must start with a capital letter", Toast.LENGTH_SHORT).show()
+                    return retBool
+                }
+            }
+        } else {
+            retBool = false
+            Toast.makeText(applicationContext, "Your must write in your full name", Toast.LENGTH_SHORT).show()
+            return retBool
+        }
+
+        return retBool
     }
 
     private fun init() {
