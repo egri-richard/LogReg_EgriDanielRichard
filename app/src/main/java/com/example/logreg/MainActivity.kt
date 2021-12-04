6207a82c1c096ef7156acf63dfe8a20acefac954
Erinad-2002 <Erinad.business@gmail.com>
2021-12-04 12:38:18 +0100

    registration

--- app/src/main/java/com/example/logreg/MainActivity.kt
+++ app/src/main/java/com/example/logreg/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.logreg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var logInText: EditText
+    private lateinit var logInPass: EditText
+    private lateinit var logInBtn: Button
+    private lateinit var regBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+
+        regBtn.setOnClickListener {
+            startActivity(Intent(applicationContext, RegisterActivity::class.java))
+            finish()
+        }
+
+        logInBtn.setOnClickListener {
+            val db = DBHandler(this)
+            if (logInText.text.isNotEmpty() && logInPass.text.isNotEmpty()) {
+                if (checkLogin()) {
+                    startActivity(Intent(applicationContext, LoggedInActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(applicationContext, "This user doesn't exist", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(applicationContext, "Please fill in every field", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkLogin(): Boolean {
+        var valid = false
+        val db = DBHandler(this)
+        val Users = db.getUsers()
+
+        for (user in Users) {
+            if (logInText.text.toString().equals(user.felhnev) && logInPass.text.toString().equals(user.jelszo)) {
+                valid = true
+            }
+        }
+
+        return valid
+    }
+
+    private fun init() {
+        logInText = findViewById(R.id.logInText)
+        logInPass = findViewById(R.id.logInPass)
+        logInBtn = findViewById(R.id.logInBtn)
+        regBtn = findViewById(R.id.regBtn)
     }
 }
--- app/src/main/java/com/example/logreg/RegisterActivity.kt
+++ app/src/main/java/com/example/logreg/RegisterActivity.kt
@@ -1,11 +1,64 @@
 package com.example.logreg
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var regEmailET : EditText
+    private lateinit var regFelhnevET : EditText
+    private lateinit var regPassET : EditText
+    private lateinit var regFullNameET : EditText
+    private lateinit var regBtn : Button
+    private lateinit var backFromRegBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        init()
+
+        backFromRegBtn.setOnClickListener {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+
+        regBtn.setOnClickListener {
+            val db = DBHandler(this)
+
+            val uName = regFelhnevET.text.toString()
+            val Email = regEmailET.text.toString()
+
+            val validUName = !db.containsUsername(uName)
+            val validEmail = !db.containsEmail(Email)
+
+            if (validUName && validEmail) {
+                db.insert(User(
+                    regFullNameET.text.toString(),
+                    regFelhnevET.text.toString(),
+                    regPassET.text.toString(),
+                    regEmailET.text.toString()
+                ))
+
+                Toast.makeText(applicationContext, "User added to database", Toast.LENGTH_SHORT).show()
+            } else {
+                if (validUName) {
+                    Toast.makeText(applicationContext, "This Email address is already in use", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(applicationContext, "This Username is already in use", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun init() {
+        regEmailET = findViewById(R.id.regEmailET)
+        regFelhnevET = findViewById(R.id.regFelhnevET)
+        regPassET = findViewById(R.id.regPassET)
+        regFullNameET = findViewById(R.id.regFullNameET)
+        regBtn = findViewById(R.id.regBtn)
+        backFromRegBtn = findViewById(R.id.backFromRegBtn)
     }
 }
